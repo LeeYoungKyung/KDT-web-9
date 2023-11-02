6e0ff167135a904752b2b34df2803db08bbba2ab
LeeYoungKyung <yklee1040@naver.com>
2023-11-02 20:01:24 +0900

    tailwind

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -4,9 +4,12 @@ import "./index.css";
 function App() {
   return (
     <>
-      <div className="bg-red-200"> TailWind CSS 적용 </div>
+      <div className="bg-red-200 text-2xl"> TailWind CSS 적용 </div>
       <div className="bg-red-300"> 성공! </div>
-      <div className="bg-sky-300 text-center">hello</div>
+      <div className="h-96 w-96 bg-cyan-400 rounded-full pb-16">
+        <div className="h-56 w-56 bg-teal-200 rounded-full place-items-center"></div>
+      </div>
+      <div className="bg-sky-300 text-center font-serif text-3xl">hello</div>
     </>
   );
 }
